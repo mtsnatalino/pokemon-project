0f356103f166fab426b492329f40b0c535127792
mtsnatalino <matheuspedro982@gmail.com>
2024-10-20 19:23:26 -0300

    Separando o consumo da API da manipulação de HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,35 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-fetch(url)
+function convertPokemonToLi(pokemon){
+
+    return `
+            <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+                    </ol>
+
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                        alt="${pokemon.name}">
+                </div>
+            </li>
+            `
+}
+
+const pokemonList = document.getElementById('pokemonList') //pegando a lista em HTML
+
+fetch(url) //fazendo a requisicao HTTP, para buscar os pokemons
     .then((response) => response.json())   //processamento assincrono
-    .then((jsonBody) => console.log(jsonBody))        
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {       
+        for(let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];      
+            pokemonList.innerHTML += convertPokemonToLi(pokemon) //convertendo a estrutura de objeto de pokemon para a estrutura LI
+        }
+    })          
     .catch((error) => console.error(error)) 
