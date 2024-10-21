64aafd7da8b3cb2bd1a839c1778666a8d977701a
mtsnatalino <matheuspedro982@gmail.com>
2024-10-21 13:25:34 -0300

    manipulando multiplas requisicoes em paralelo

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,8 +1,25 @@
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-    return fetch(url) //fazendo a requisicao HTTP, para buscar os pokemons
-        .then((response) => response.json())   //processamento assincrono
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+   
+    return fetch(url)                                //fazendo a requisicao HTTP, para buscar os pokemons
+        .then((response) => response.json())         //processamento assincrono
         .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonDetails)=>  pokemonDetails)
 }   
+
+// Promise.all([
+//     fetch('https://pokeapi.co/api/v2/pokemon/1'),
+//     fetch('https://pokeapi.co/api/v2/pokemon/2'),
+//     fetch('https://pokeapi.co/api/v2/pokemon/3'),
+//     fetch('https://pokeapi.co/api/v2/pokemon/4')
+// ]).then((results) => {
+//     console.log(results)
+// })
